7aa9c9feac5c837614f237e2462027e3a517f59e
Vladyslav Bocharov <vladislavboc11@gmail.com>
2024-11-12 18:06:52 +0200

    Added DbContext with start classes and connected SQL Server

--- MediaHub.API/Program.cs
+++ MediaHub.API/Program.cs
@@ -1,3 +1,10 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using MediaHub.EntityFramewok;
+using MediaHub.Models;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace MediaHub.API
 {
@@ -13,6 +20,42 @@ namespace MediaHub.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddIdentityApiEndpoints<User>()
+                .AddEntityFrameworkStores<DataContext>();
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen(option =>
+            {
+                option.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization header using the Bearer scheme (\"Bearer {token}\")",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                option.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+
+            //builder.Services.AddDbContext<DataContext>(options => {
+            //    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+            //});
+            builder.Services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("MediaHub.EntityFramework"));
+            });
+
+            // Enable CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllHeaders",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyHeader()
+                               .AllowAnyMethod();
+                    });
+            });
 
             var app = builder.Build();
 
@@ -23,12 +66,16 @@ namespace MediaHub.API
                 app.UseSwaggerUI();
             }
 
+            app.MapIdentityApi<User>();
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
 
             app.MapControllers();
+            // Cors
+            app.UseCors("AllowAllHeaders");
 
             app.Run();
         }
---
+++ MediaHub.EntityFramework/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using MediaHub.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaHub.EntityFramewok
+{
+    public class DataContext : IdentityDbContext<User, Role, string>
+    {
+        public DataContext() { }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public override DbSet<User> Users { get; set; }
+        public override DbSet<Role> Roles { get; set; }
+    }
+}
